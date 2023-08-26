89ecd33e29c7de736da66d4508a0f8e05e2d6d8b
OSCARS26 <oscarolivare1@gmail.com>
2023-08-26 00:55:14 -0400

    cambios

--- app/components/registro/registro.component.ts
+++ app/components/registro/registro.component.ts
@@ -1,12 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.scss'],
 })
-export class RegistroComponent {
+export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  formularioRegistro: FormGroup;
+
+  constructor(public fb: FormBuilder) { 
+    this.formularioRegistro = this.fb.group({
+      'nombre': new FormControl("", Validators.required),
+      'correo': new FormControl("", Validators.required),
+      'password': new FormControl("", Validators.required),
+      'confirmacionPassword': new FormControl("", Validators.required)
+    });
+  }
+
+  guardar() {
+    if (this.formularioRegistro.valid) {
+      // Aquí puedes agregar la lógica para guardar el registro
+      console.log('Formulario válido, se puede guardar');
+    } else {
+      console.log('Formulario inválido, no se puede guardar');
+    }
+  }
+
+  ngOnInit() {}
 
 }
--- app/pages/registro/registro.page.ts
+++ app/pages/registro/registro.page.ts
@@ -1,42 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { 
-  FormGroup,
-  FormControl,
-  Validators,
-  FormBuilder
-} from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-registro',
-  templateUrl: './registro.page.html',
-  styleUrls: ['./registro.page.scss'],
+  selector: 'app-registro', // Asegúrate de que el selector coincide con el nombre de la página
+  templateUrl: 'registro.page.html',
+  styleUrls: ['registro.page.scss'],
 })
-export class RegistroPage implements OnInit {
+export class RegistroPage { // Cambia el nombre de la clase a RegistroPage
 
-  formularioRegistro: FormGroup;
-
-  //Formulario de Registro//
-  constructor(public fb: FormBuilder) { 
-    this.formularioRegistro = this.fb.group({
-      'nombre': new FormControl("", Validators.required),
-      'correo': new FormControl("", Validators.required),
-      'password': new FormControl("", Validators.required),
-      'confirmacionPassword': new FormControl("", Validators.required)
-    })
-  }
-
-  //Verificador del Formulario//
-  guardar() {
-    if (this.formularioRegistro.valid) {
-      // Aquí puedes agregar la lógica para guardar el registro
-      console.log('Formulario válido, se puede guardar');
-    } else {
-      console.log('Formulario inválido, no se puede guardar');
-    }
-  }
-
-  ngOnInit() {
-
-  }
+  constructor() {}
 
 }
